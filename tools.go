ed5fcdd5ab88d70fd1e3b15f6bc0371b21e25b1e
Travis Cline <travis.cline@gmail.com>
2021-05-16 17:34:44 -0700

    build: Update to new esbuild build process

--- tools.go
+++ tools.go
@@ -4,6 +4,7 @@ package goloz
 
 import (
 	_ "github.com/bufbuild/buf/cmd/buf"
+	_ "github.com/evanw/esbuild/cmd/esbuild"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
